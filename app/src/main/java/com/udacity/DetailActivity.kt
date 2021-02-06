e476add4c67040f9c0b07c912f9c890766166852
John Vieira <jvieira575@gmail.com>
2021-02-06 13:18:46 -0600

    Created companion object to create an intent for the detail activity. Temporarily added code to start the detail activity from the main activity to test the detail activity processing and animation.

--- app/src/main/java/com/udacity/DetailActivity.kt
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,15 +1,52 @@
 package com.udacity
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_detail.*
 
+/**
+ * The detail [AppCompatActivity] of the application. It will display the file downloaded and the status
+ * of the download. It also animates the views when doing so.
+ */
 class DetailActivity : AppCompatActivity() {
+
+    companion object {
+
+        private const val INTENT_FILE_NAME_KEY = "INTENT_FILE_NAME_KEY"
+        private const val INTENT_DOWNLOAD_STATUS_KEY = "INTENT_DOWNLOAD_STATUS_KEY"
+
+        fun newIntent(context: Context, fileName :String, downloadStatus : String): Intent {
+
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(INTENT_FILE_NAME_KEY, fileName)
+            intent.putExtra(INTENT_DOWNLOAD_STATUS_KEY, downloadStatus)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+            return intent
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
-    }
 
+        // Retrieve the Intent that started this activity and extract the file name and download status
+        val fileName = intent.getStringExtra(INTENT_FILE_NAME_KEY) ?: "Unknown File Name"
+        val downloadStatus = intent.getStringExtra(INTENT_DOWNLOAD_STATUS_KEY) ?: "Unknown Status"
+
+        // Set the file name and download status
+        file_name_text_view.text = fileName
+        download_status_text_view.text = downloadStatus
+
+        // Register button listener to navigate to Main Activity
+        ok_button.setOnClickListener {
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 }
--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -14,7 +14,11 @@ import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
-
+/**
+ * The main [AppCompatActivity] of the application. Allows the user to select one of three GIT repos to download from.
+ * Once a repo is selected, and the download button is clicked, the button will animate and a download
+ * will commence. A notification will appear allowing the user to click to the detail view.
+ */
 class MainActivity : AppCompatActivity() {
 
     private var downloadID: Long = 0
@@ -28,22 +32,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        // Register our receiver
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
+        // Register the click listener
         custom_button.setOnClickListener {
-            download()
+
+            // Temporary. Just testing navigation and detail activity
+            val detailActivityIntent = DetailActivity.newIntent(this, INTENT_FILE_NAME_VALUE, INTENT_DOWNLOAD_STATUS_VALUE)
+            startActivity(detailActivityIntent)
         }
     }
 
+    /**
+     * The [BroadcastReceiver] that is invoked when the download is complete.
+     */
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
         }
     }
 
+    /**
+     * Function that initiates a download of the selected GIT repository.
+     */
     private fun download() {
         val request =
-            DownloadManager.Request(Uri.parse(URL))
+            DownloadManager.Request(Uri.parse(URL_LOAD_APP_REPO))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
                 .setRequiresCharging(false)
@@ -55,10 +70,18 @@ class MainActivity : AppCompatActivity() {
             downloadManager.enqueue(request)// enqueue puts the download request in the queue.
     }
 
+    /**
+     * Companion object that contains constants for use in the activity.
+     */
     companion object {
-        private const val URL =
-            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val TOAST_MESSAGE = "Please select the file to download"
+        private const val URL_GLIDE_REPO = "https://github.com/bumptech/glide/archive/master.zip"
+        private const val URL_LOAD_APP_REPO = "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val URL_RETROFIT_REPO = "https://github.com/square/retrofit/archive/master.zip"
         private const val CHANNEL_ID = "channelId"
-    }
 
+        // Temporary values to test detail screen
+        private const val INTENT_FILE_NAME_VALUE = "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val INTENT_DOWNLOAD_STATUS_VALUE = "Success"
+    }
 }
